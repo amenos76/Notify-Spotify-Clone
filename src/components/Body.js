7f372d918270affedcc71fec051ed79424ffe490
Augie Menos <augie.menos@gmail.com>
2021-01-27 14:18:21 -0700

    discover weekly image and text

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,15 @@ export default function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+
+      <div className="body-info">
+        <img src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png" alt="discover-weekly" />
+        <div className="body-infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>description....</p>
+        </div>
+      </div>
     </div>
   )
 }
